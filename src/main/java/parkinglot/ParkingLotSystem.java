33b96f01820f154e197eaf415fd425ff80ac0c83
Pritamsuryawanshi <pritam.surya110@gmail.com>
2020-03-23 14:48:47 +0530

    ADDED: Code and test to perform car parking

--- src/main/java/parkinglot/ParkingLotSystem.java
+++ src/main/java/parkinglot/ParkingLotSystem.java
@@ -1,4 +1,8 @@
 package parkinglot;
 //Welcome to parking lot system
 public class ParkingLotSystem {
+
+    public boolean park(Object vehicle) {
+        return true;
+    }
 }
--- src/test/java/parkinglot/ParkingLotTest.java
+++ src/test/java/parkinglot/ParkingLotTest.java
@@ -1,10 +1,13 @@
 package parkinglot;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ParkingLotTest {
     @Test
     public void givenAVehicle_WhenParked_ShouldBeAbleToPark() {
         ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
+        boolean parked = parkingLotSystem.park(new Object());
+        Assert.assertTrue(parked);
     }
 }
